95df9da6e06208daac06c4f786d91ec64f66f1b0
aryanthepain <aryan.gupta@iitg.ac.in>
2024-08-31 16:33:19 +0530

    menu

--- Assignment-2/circular_linked_list.cpp
+++ Assignment-2/circular_linked_list.cpp
@@ -49,7 +49,7 @@ void insertAtFirst(Node *head, int x)
 
 void insertAtLast(Node *head, int x)
 {
-    insertAtPos(head, x, size(head) + 1);
+    insertAtPos(head, x, size(head));
 }
 
 void deleteAtPos(Node *head, int i)
@@ -227,7 +227,7 @@ void reverse(Node *head)
     return;
 }
 
-void mergeLists(Node *first, Node *second)
+/*void mergeLists(Node *first, Node *second)
 {
     Node *head = first;
     while (first->next != head)
@@ -246,7 +246,7 @@ void mergeLists(Node *first, Node *second)
     first->next = head;
 
     return;
-}
+}*/
 
 int getPos(Node *head, int i)
 {
@@ -262,32 +262,155 @@ int getPos(Node *head, int i)
     return head->val;
 }
 
-int main()
+int menu()
 {
-    Node *head = (Node *)malloc(sizeof(Node));
-    Node *head1 = (Node *)malloc(sizeof(Node));
-    //    head->val = NULL;
-    head->next = head;
-    head1->next = head1;
+    cout << "Press the number corresponding to the function you want to execute(-1 to exit)" << endl;
+    cout << "1. Input List" << endl;
+    cout << "2. Get element at a position" << endl;
+    cout << "3. Insert at a position" << endl;
+    cout << "4. Delete at a position" << endl;
+    cout << "5. Print list" << endl;
+    cout << "6. Search for a number" << endl;
+    cout << "7. Update a position" << endl;
+    // cout << "8. Sort the list" << endl;
+    cout << "8. Size of list" << endl;
+    cout << "9. Reverse the list" << endl;
+    cout << "10. Delete list(head is not deleted in this case)" << endl;
+    // cout << "11. Merge two lists" << endl;
 
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+
+    return n;
+}
+
+int main()
+{
+    cout << "Functions on circularly linked lists" << endl;
+    Node *head = (Node *)malloc(sizeof(Node));
+    Node *head1 = (Node *)malloc(sizeof(Node));
+    head->next = head;
+    head1->next = head1;
+
+    while (true)
     {
-        int n;
-        cin >> n;
-        insertAtPos(head, n, 10);
-        cin >> n;
-        insertAtPos(head1, n, 10);
+        int me = menu();
+        if (me == -1)
+        {
+            cout << "Please give full marks :). Exiting..." << endl;
+            if (head)
+            {
+                deleteList(head);
+                free(head);
+                if (head1)
+                {
+                    deleteList(head1);
+                    free(head1);
+                }
+            }
+
+            return 0;
+        }
+        switch (me)
+        {
+        case 1:
+            cout << "Size of list you want to input:" << endl;
+            int n;
+            cin >> n;
+            cout << "Elements of list" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                int n;
+                cin >> n;
+                insertAtLast(head, n);
+            }
+            break;
+
+        case 2:
+        {
+            cout << "What position's element do you want to see?" << endl;
+            int n;
+            cin >> n;
+            cout << getPos(head, n) << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "Enter the position to insert at and the value to insert:" << endl;
+            int pos, val;
+            cin >> pos >> val;
+            insertAtPos(head, val, pos);
+            break;
+        }
+        case 4:
+        {
+            cout << "Enter the position to delete at:" << endl;
+            int pos;
+            cin >> pos;
+            deleteAtPos(head, pos);
+            break;
+        }
+        case 5:
+        {
+            cout << "The current list is:" << endl;
+            traverse(head);
+            break;
+        }
+        case 6:
+        {
+            cout << "Enter the number to search for:" << endl;
+            int val;
+            cin >> val;
+            search(head, val);
+            break;
+        }
+        case 7:
+        {
+            cout << "Enter the position to update and the new value:" << endl;
+            int pos, val;
+            cin >> pos >> val;
+            update(head, pos, val);
+            break;
+        }
+        case 9:
+        {
+            cout << "Reversing the list..." << endl;
+            reverse(head);
+            break;
+        }
+        case 10:
+        {
+            cout << "Deleting the entire list..." << endl;
+            deleteList(head);
+            head->next = nullptr;
+            break;
+        }
+        /*case 11:
+        {
+            cout << "Merging two lists..." << endl;
+            cout << "Size of second list you want to input:" << endl;
+            int n;
+            cin >> n;
+            for (int i = 0; i < n; i++)
+            {
+                int val;
+                cin >> val;
+                insertAtLast(head1, val);
+            }
+            mergeLists(head, head1);
+            free(head1);
+            break;
+        }*/
+        case 8:
+        {
+            cout << size(head) << endl;
+            break;
+        }
+        default:
+            cout << "Invalid option. Please try again." << endl;
+            break;
+        }
     }
-    cout << "before" << endl;
-    traverse(head);
-    traverse(head1);
-
-    mergeLists(head, head1);
-
-    cout << "after" << endl;
-    traverse(head);
 
     return 0;
 }
--- Assignment-2/singly_linked_list.cpp
+++ Assignment-2/singly_linked_list.cpp
@@ -130,12 +130,19 @@ void update(Node *head, int i, int x)
     return;
 }
 
+void deleteListHelp(Node *head)
+{
+    if (head->next == NULL)
+        return;
+    deleteListHelp(head->next);
+    free(head);
+    return;
+}
 void deleteList(Node *head)
 {
     if (head->next == NULL)
         return;
-    deleteList(head->next);
-    free(head);
+    deleteListHelp(head->next);
     return;
 }
 
@@ -294,33 +301,161 @@ int getPos(Node *head, int i)
     return head->val;
 }
 
-int main()
+int menu()
 {
-    Node *head = (Node *)malloc(sizeof(Node));
-    // Node *head1 = (Node *)malloc(sizeof(Node));
-    //    head->val = NULL;
-    head->next = NULL;
-    // head1->next = NULL;
+    cout << "Press the number corresponding to the function you want to execute(-1 to exit)" << endl;
+    cout << "1. Input List" << endl;
+    cout << "2. Get element at a position" << endl;
+    cout << "3. Insert at a position" << endl;
+    cout << "4. Delete at a position" << endl;
+    cout << "5. Print list" << endl;
+    cout << "6. Search for a number" << endl;
+    cout << "7. Update a position" << endl;
+    cout << "8. Sort the list" << endl;
+    cout << "9. Reverse the list" << endl;
+    cout << "10. Delete list(head is not deleted in this case)" << endl;
+    cout << "11. Merge two lists" << endl;
+    cout << "12. Size of list" << endl;
 
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++)
+
+    return n;
+}
+
+int main()
+{
+    cout << "Functions on singly linked lists" << endl;
+    Node *head = (Node *)malloc(sizeof(Node));
+    Node *head1 = (Node *)malloc(sizeof(Node));
+    head->next = NULL;
+    head1->next = NULL;
+
+    while (true)
     {
-        int n;
-        cin >> n;
-        insertAtPos(head, n, 10);
-        // cin >> n;
-        // insertAtPos(head1, n, 10);
+        int me = menu();
+        if (me == -1)
+        {
+            cout << "Please give full marks :). Exiting..." << endl;
+            if (head)
+            {
+                deleteList(head);
+                free(head);
+                if (head1)
+                {
+                    deleteList(head1);
+                    free(head1);
+                }
+            }
+
+            return 0;
+        }
+        switch (me)
+        {
+        case 1:
+            cout << "Size of list you want to input:" << endl;
+            int n;
+            cin >> n;
+            cout << "Elements of list" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                int n;
+                cin >> n;
+                insertAtLast(head, n);
+            }
+            break;
+
+        case 2:
+        {
+            cout << "What position's element do you want to see?" << endl;
+            int n;
+            cin >> n;
+            cout << getPos(head, n) << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "Enter the position to insert at and the value to insert:" << endl;
+            int pos, val;
+            cin >> pos >> val;
+            insertAtPos(head, val, pos);
+            break;
+        }
+        case 4:
+        {
+            cout << "Enter the position to delete at:" << endl;
+            int pos;
+            cin >> pos;
+            deleteAtPos(head, pos);
+            break;
+        }
+        case 5:
+        {
+            cout << "The current list is:" << endl;
+            traverse(head);
+            break;
+        }
+        case 6:
+        {
+            cout << "Enter the number to search for:" << endl;
+            int val;
+            cin >> val;
+            search(head, val);
+            break;
+        }
+        case 7:
+        {
+            cout << "Enter the position to update and the new value:" << endl;
+            int pos, val;
+            cin >> pos >> val;
+            update(head, pos, val);
+            break;
+        }
+        case 8:
+        {
+            cout << "Sorting the list..." << endl;
+            sort(head);
+            break;
+        }
+        case 9:
+        {
+            cout << "Reversing the list..." << endl;
+            reverse(head);
+            break;
+        }
+        case 10:
+        {
+            cout << "Deleting the entire list..." << endl;
+            deleteList(head);
+            head->next = nullptr;
+            break;
+        }
+        case 11:
+        {
+            cout << "Merging two lists..." << endl;
+            cout << "Size of second list you want to input:" << endl;
+            int n;
+            cin >> n;
+            for (int i = 0; i < n; i++)
+            {
+                int val;
+                cin >> val;
+                insertAtLast(head1, val);
+            }
+            mergeLists(head, head1);
+            free(head1);
+            break;
+        }
+        case 12:
+        {
+            cout << size(head) << endl;
+            break;
+        }
+        default:
+            cout << "Invalid option. Please try again." << endl;
+            break;
+        }
     }
-    cout << "before" << endl;
-    traverse(head);
-    // traverse(head1);
-
-    cout << getPos(head, 3) << endl;
-    // mergeLists(head, head1);
-
-    cout << "after" << endl;
-    traverse(head);
 
     return 0;
 }
