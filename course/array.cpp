dfeb44c3c99ba72fb5a3154ecfa4cfacab8d3e8c
aryanthepain <aryan.gupta@iitg.ac.in>
2024-11-15 15:24:40 +0530

    string in array

--- course/array.cpp
+++ course/array.cpp
@@ -1,97 +1,128 @@
 #include "include/Array.h"
 
-// Constructors
-Array Array::zeros(size_t size)
+// Template specializations for double
+template <>
+Array<double> Array<double>::zeros(size_t size)
 {
-    return Array(vector<double>(size, 0));
+    return Array<double>(vector<double>(size, 0.0));
 }
 
-Array Array::ones(size_t size)
+template <>
+Array<double> Array<double>::ones(size_t size)
 {
-    return Array(vector<double>(size, 1));
+    return Array<double>(vector<double>(size, 1.0));
 }
 
-Array Array::arange(double start, double end, double step)
+template <>
+Array<double> Array<double>::arange(double start, double end, double step)
 {
     vector<double> data;
     for (double i = start; i < end; i += step)
     {
         data.push_back(i);
     }
-    return Array(data);
+    return Array<double>(data);
 }
 
-Array Array::linspace(double start, double end, size_t num)
+template <>
+Array<double> Array<double>::linspace(double start, double end, size_t num)
 {
     vector<double> data(num);
     for (size_t i = 0; i < num; ++i)
     {
         data[i] = start + (end - start) * (i / static_cast<double>(num - 1));
     }
-    return Array(data);
+    return Array<double>(data);
+}
+
+// Specialization for string
+template <>
+Array<string> Array<string>::zeros(size_t size)
+{
+    return Array<string>(vector<string>(size, "0"));
+}
+
+template <>
+Array<string> Array<string>::ones(size_t size)
+{
+    return Array<string>(vector<string>(size, "1"));
 }
 
 // Array Manipulation
-Array Array::concatenate(const Array &a, const Array &b)
+template <typename T>
+Array<T> Array<T>::concatenate(const Array<T> &a, const Array<T> &b)
 {
-    vector<double> newData = a.data;
+    vector<T> newData = a.data;
     newData.insert(newData.end(), b.data.begin(), b.data.end());
-    return Array(newData);
+    return Array<T>(newData);
 }
 
-vector<Array> Array::split(const Array &a, size_t num)
+template <typename T>
+vector<Array<T>> Array<T>::split(const Array<T> &a, size_t num)
 {
-    vector<Array> result;
+    vector<Array<T>> result;
     size_t splitSize = a.data.size() / num;
     for (size_t i = 0; i < num; ++i)
     {
-        vector<double> subData(a.data.begin() + i * splitSize, a.data.begin() + (i + 1) * splitSize);
-        result.push_back(Array(subData));
+        vector<T> subData(a.data.begin() + i * splitSize, a.data.begin() + (i + 1) * splitSize);
+        result.push_back(Array<T>(subData));
     }
     return result;
 }
 
-// Mathematical Operations
-Array Array::add(const Array &other)
+// Mathematical Operations for double
+template <>
+Array<double> Array<double>::add(const Array<double> &other)
 {
     return elementWiseOperation(other, [](double a, double b)
                                 { return a + b; });
 }
-Array Array::subtract(const Array &other)
+
+template <>
+Array<double> Array<double>::subtract(const Array<double> &other)
 {
     return elementWiseOperation(other, [](double a, double b)
                                 { return a - b; });
 }
-Array Array::multiply(const Array &other)
+
+template <>
+Array<double> Array<double>::multiply(const Array<double> &other)
 {
     return elementWiseOperation(other, [](double a, double b)
                                 { return a * b; });
 }
-Array Array::divide(const Array &other)
+
+template <>
+Array<double> Array<double>::divide(const Array<double> &other)
 {
     return elementWiseOperation(other, [](double a, double b)
                                 { return a / b; });
 }
-Array Array::power(double exponent)
+
+template <>
+Array<double> Array<double>::power(double exponent)
 {
     return elementWiseOperation([exponent](double a)
                                 { return pow(a, exponent); });
 }
 
-double Array::sum()
+template <>
+double Array<double>::sum()
 {
-    return accumulate(data.begin(), data.end(), 0);
+    return accumulate(data.begin(), data.end(), 0.0);
 }
 
-double Array::mean()
+template <>
+double Array<double>::mean()
 {
     return sum() / data.size();
 }
 
-double Array::std()
+template <>
+double Array<double>::std()
 {
     double meanVal = mean();
-    double accum = 0;
+    double accum = 0.0;
     for (double d : data)
     {
         accum += (d - meanVal) * (d - meanVal);
@@ -99,44 +130,53 @@ double Array::std()
     return sqrt(accum / data.size());
 }
 
-// Random Sampling
-Array Array::randomRand(size_t size)
+// Random Sampling for double
+template <>
+Array<double> Array<double>::randomRand(size_t size)
 {
     vector<double> data(size);
     generate(data.begin(), data.end(), []()
              { return static_cast<double>(rand()) / RAND_MAX; });
-    return Array(data);
+    return Array<double>(data);
 }
 
 // Print function
-void Array::print() const
+template <typename T>
+void Array<T>::print() const
 {
-    for (const double &val : data)
+    for (const T &val : data)
     {
-        cout << fixed << setprecision(2) << val << " ";
+        cout << val << " ";
     }
     cout << endl;
 }
 
-Array Array::elementWiseOperation(const Array &other, function<double(double, double)> op)
+// Element-wise operations
+template <typename T>
+Array<T> Array<T>::elementWiseOperation(const Array<T> &other, function<T(T, T)> op)
 {
     if (data.size() != other.data.size())
     {
         throw invalid_argument("Arrays must be of the same size.");
     }
-    vector<double> result(data.size());
+    vector<T> result(data.size());
     for (size_t i = 0; i < data.size(); ++i)
     {
         result[i] = op(data[i], other.data[i]);
     }
-    return Array(result);
+    return Array<T>(result);
 }
 
-Array Array::elementWiseOperation(function<double(double)> op)
+template <typename T>
+Array<T> Array<T>::elementWiseOperation(function<T(T)> op)
 {
-    vector<double> result(data.size());
+    vector<T> result(data.size());
     for (size_t i = 0; i < data.size(); ++i)
-
+    {
         result[i] = op(data[i]);
-    return Array(result);
+    }
+    return Array<T>(result);
 }
+
+// Note: For string operations, you may want to implement specific functions as needed,
+// since mathematical operations like add, subtract, etc., do not apply to strings.
--- course/include/array.h
+++ course/include/array.h
@@ -4,44 +4,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
 class Array
 {
 private:
-    vector<double> data;
+    vector<T> data;
 
-    Array elementWiseOperation(const Array &other, function<double(double, double)> op);
-    Array elementWiseOperation(function<double(double)> op);
+    Array<T> elementWiseOperation(const Array<T> &other, function<T(T, T)> op);
+    Array<T> elementWiseOperation(function<T(T)> op);
 
 public:
     // Constructors
-    Array(const vector<double> &data) : data(data) {}
-    static Array zeros(size_t size);
-    static Array ones(size_t size);
-    static Array arange(double start, double end, double step);
-    static Array linspace(double start, double end, size_t num);
+    Array(const vector<T> &data) : data(data) {}
+    static Array<T> zeros(size_t size);
+    static Array<T> ones(size_t size);
+    static Array<T> arange(double start, double end, double step);
+    static Array<T> linspace(double start, double end, size_t num);
 
     // Array Manipulation
-    static Array concatenate(const Array &a, const Array &b);
-    static vector<Array> split(const Array &a, size_t num);
+    static Array<T> concatenate(const Array<T> &a, const Array<T> &b);
+    static vector<Array<T>> split(const Array<T> &a, size_t num);
 
     // Mathematical Operations
-    Array add(const Array &other);
-    Array subtract(const Array &other);
-    Array multiply(const Array &other);
-    Array divide(const Array &other);
-    Array power(double exponent);
+    Array<T> add(const Array<T> &other);
+    Array<T> subtract(const Array<T> &other);
+    Array<T> multiply(const Array<T> &other);
+    Array<T> divide(const Array<T> &other);
+    Array<T> power(double exponent);
     double sum();
     double mean();
     double std();
 
     // Random Sampling
-    static Array randomRand(size_t size);
+    static Array<T> randomRand(size_t size);
 
     // Print function
     void print() const;
 
-    // return the data
-    vector<double> getData() const
+    // Return the data
+    vector<T> getData() const
     {
         return data;
     }
