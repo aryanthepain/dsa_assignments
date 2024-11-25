826ba1f0770332a628420b25269361c144f18679
aryanthepain <aryan.gupta@iitg.ac.in>
2024-11-26 03:59:56 +0530

    statistical methods tested

--- course/testing.cpp
+++ course/testing.cpp
@@ -6,9 +6,26 @@ int main()
 
     df.print();
 
-    DataFrame copydf = df.copy();
-
-    copydf.print();
+    cout << df.sum(1) << endl;
+    cout << df.mean(1) << endl;
+    auto [q1, q2, q3] = df.quartiles(2);
+    cout << q1 << endl;
+    try
+    {
+        df.mean(0);
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        df.mean(5);
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
