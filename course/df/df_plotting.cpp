44ba579f1b4ee931196707f283cfe85aadb13a62
aryanthepain <aryan.gupta@iitg.ac.in>
2024-11-26 04:52:19 +0530

    plotting methods added

---
+++ course/df/df_plotting.cpp
@@ -0,0 +1,18 @@
+// author: Aryanthepain
+#include "../include/df.h"
+
+// Plotting methods (placeholders)
+void DataFrame::plot() const
+{
+    // Implementation for basic plotting
+}
+
+void DataFrame::hist(size_t col) const
+{
+    // Implementation for generating a histogram
+}
+
+void DataFrame::boxplot(size_t col) const
+{
+    // Implementation for creating a box plot
+}
