55dc7d37072e70bd54dd5daeaf8236c265be7eb5
aryanthepain <aryan.gupta@iitg.ac.in>
2024-11-26 20:30:23 +0530

    plot data tested

--- course/df/df_plotting.cpp
+++ course/df/df_plotting.cpp
@@ -1,18 +1,117 @@
 // author: Aryanthepain
 #include "../include/df.h"
 
-// Plotting methods (placeholders)
 void DataFrame::plot() const
 {
-    // Implementation for basic plotting
+    for (size_t i = 0; i < columns.size(); ++i)
+    {
+        std::cout << "Plotting column " << i << std::endl;
+
+        if (holds_alternative<Array<double>>(columns[i]))
+        {
+            const auto &array = get<Array<double>>(columns[i]);
+            const auto &data = array.getData();
+
+            // Write data to a temporary file
+            std::ofstream outFile("temp_data.txt");
+            for (size_t j = 0; j < data.size(); ++j)
+            {
+                outFile << j << " " << data[j] << std::endl; // x, y format
+            }
+            outFile.close();
+
+            // Call gnuplot to plot the data
+            std::string command =
+                "gnuplot -e \"set terminal png; "
+                "set output 'plots/plot_" +
+                std::to_string(i) + ".png'; "
+                                    "set title 'Plot of Column " +
+                std::to_string(i) + "'; "
+                                    "set xlabel 'Index'; "
+                                    "set ylabel 'Value'; "
+                                    "plot 'temp_data.txt' with lines title 'Column " +
+                std::to_string(i) + "';\"";
+            std::system(command.c_str());
+        }
+    }
 }
 
 void DataFrame::hist(size_t col) const
 {
-    // Implementation for generating a histogram
+    if (col < columns.size())
+    {
+        std::cout << "Generating histogram for column " << col << std::endl;
+
+        if (holds_alternative<Array<double>>(columns[col]))
+        {
+            const auto &array = get<Array<double>>(columns[col]);
+            const auto &data = array.getData();
+
+            // Write data to a temporary file
+            std::ofstream outFile("temp_data.txt");
+            for (const auto &value : data)
+            {
+                outFile << value << std::endl;
+            }
+            outFile.close();
+
+            // Call gnuplot to generate the histogram
+            std::string command =
+                "gnuplot -e \"set terminal png; "
+                "set output 'plots/histogram_" +
+                std::to_string(col) + ".png'; "
+                                      "set title 'Histogram of Column " +
+                std::to_string(col) + "'; "
+                                      "set xlabel 'Value'; "
+                                      "set ylabel 'Frequency'; "
+                                      "binwidth=1; "
+                                      "bin(x,width)=width*floor(x/width); "
+                                      "set style fill solid; "
+                                      "set boxwidth binwidth; "
+                                      "plot 'temp_data.txt' using (bin($1,binwidth)):(1.0) smooth freq with boxes title 'Frequency';\"";
+            std::system(command.c_str());
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid column index" << std::endl;
+    }
 }
 
 void DataFrame::boxplot(size_t col) const
 {
-    // Implementation for creating a box plot
+    if (col < columns.size())
+    {
+        std::cout << "Creating box plot for column " << col << std::endl;
+
+        if (holds_alternative<Array<double>>(columns[col]))
+        {
+            const auto &array = get<Array<double>>(columns[col]);
+            const auto &data = array.getData();
+
+            // Write data to a temporary file
+            std::ofstream outFile("temp_data.txt");
+            for (const auto &value : data)
+            {
+                outFile << value << std::endl;
+            }
+            outFile.close();
+
+            // Call gnuplot to create the box plot
+            std::string command =
+                "gnuplot -e \"set terminal png; "
+                "set output 'plots/boxplot_" +
+                std::to_string(col) + ".png'; "
+                                      "set title 'Box Plot of Column " +
+                std::to_string(col) + "'; "
+                                      "set style fill solid; "
+                                      "plot 'temp_data.txt' using 1:1 with boxplot title 'Column " +
+                std::to_string(col) + "';\"";
+            std::system(command.c_str());
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid column index" << std::endl;
+    }
 }
--- course/testing.cpp
+++ course/testing.cpp
@@ -6,58 +6,9 @@ int main()
 
     df.print();
 
-    vector<bool> missingValuesA = df.is_na(1);
-    vector<bool> missingValuesB = df.is_na(2);
-
-    for (bool value : missingValuesA)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
-
-    for (bool value : missingValuesB)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
-    df.print();
-
-    df.fillna(2, 0.0);
-    df.fillna(1, "Unknown");
-    df.print();
-
-    missingValuesA = df.is_na(1);
-    missingValuesB = df.is_na(2);
-
-    for (bool value : missingValuesA)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
-
-    for (bool value : missingValuesB)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
-
-    df.dropna(1);
-    df.print();
-
-    missingValuesA = df.is_na(1);
-    missingValuesB = df.is_na(2);
-
-    for (bool value : missingValuesA)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
-
-    for (bool value : missingValuesB)
-    {
-        cout << value << " ";
-    }
-    cout << endl;
+    df.plot();
+    df.hist(1);
+    df.boxplot(2);
 
     return 0;
 }
